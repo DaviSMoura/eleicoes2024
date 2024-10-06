773215cc9acadb77696fe22e55b3edad1117b4d5
Davi Moura <davisilvag4mer@gmail.com>
2024-10-06 13:25:48 -0300

    feat: footer

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,14 @@ import ElectionResults from "./components/electionResults";
 
 export default function Home() {
   return (
-    <ElectionResults />
+    <>
+      <ElectionResults />
+      <footer style={{ position: "fixed", bottom: 0, left: "50%", transform: "translateX(-50%)", padding: "5px" }}>
+        {/* <span style={{ "background-color": "yellow" }}> */}
+        <span className="font-semibold text-sm">
+          by @davimoura.dev
+        </span>
+      </footer>
+    </>
   );
 }
